b57eee7f7f118c690a4a0bdbbbb1eaaba86f5c95
IceGate2007E <56272992+IceGate2007E@users.noreply.github.com>
2021-08-10 00:43:16 -0300

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	deleted:    src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
+import Home from './components/Home'
+import Civilization from './components/Civilization';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container mt-3">
+        <div>
+          <Link to="/" className="btn btn-dark">
+            Home
+          </Link>
+        </div>
+        <hr />
+        <Switch>
+          <Route path="/:id">
+            <Civilization />
+          </Route>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    
   );
 }
 
